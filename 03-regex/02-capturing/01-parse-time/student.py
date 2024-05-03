35a0b0ff9d6e4f12672850320dbe7d78ef35f63f
IVAN-JIANG <ivanjiang.@student.ucll.be>
2024-05-03 13:39:23 +0200

    a

---
+++ 03-regex/02-capturing/01-parse-time/student.py
@@ -0,0 +1,9 @@
+import re
+
+
+def parse_time(string):
+    match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(\.\d{3})?', string)
+
+    if match:
+        h, m, s, ms = match.groups()
+        ms = ms or '.000'
